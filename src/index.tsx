8ec5eb2004aa5f8d0158389acdbd8d2abe504dbb
Shubham Chawla <shubham_chawla@live.com>
2024-06-01 15:08:56 -0700

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import './index.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import Dashboard from 'pages/Dashboard';
+import Login from 'pages/Login';
+import NotFound from 'pages/NotFound';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+        <Route path="" element={<Navigate to="/dashboard" />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import { Container } from '@mui/material';
+
+export const Dashboard = () => {
+  return <Container maxWidth="sm">Hello</Container>;
+};
+
+export default Dashboard;
